7ba10ed1b5a2fca6b2b01977dfeba2f9d0bc002e
alex <camward@yandex.ru>
2018-09-05 23:15:22 +0400

    авторизация. сервис получения данных

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
+import { UsersService } from './shared/services/users.service';
 
 @NgModule({
   declarations: [
@@ -11,10 +13,11 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
