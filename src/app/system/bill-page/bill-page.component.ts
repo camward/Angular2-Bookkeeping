8b7283466f882fe76df73706e468fe9575a30a35
alex <camward@yandex.ru>
2018-09-09 00:13:36 +0400

    счет. вывод данных

--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -11,21 +11,41 @@ import { Bill } from '../shared/models/bill.model';
   styleUrls: ['./bill-page.component.scss']
 })
 export class BillPageComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
+  sub1: Subscription;
+  sub2: Subscription;
+
+  currency: any;
+  bill: Bill;
+
+  isLoaded = false;
 
   constructor(private billService: BillService) { }
 
   ngOnInit() {
-    this.subscription = Observable.combineLatest(
+    this.sub1 = Observable.combineLatest(
       this.billService.getBill(),
-      this.billService.getCurrency(),
+      this.billService.getCurrency()
     ).subscribe((data: [Bill, any]) => {
-      console.log(data);
+      this.bill = data[0];
+      this.currency = data[1];
+      this.isLoaded = true;
+      console.log(this.currency);
     });
   }
 
+  onRefresh() {
+    this.isLoaded = false;
+    this.sub2 = this.billService.getCurrency()
+      .delay(1000)
+      .subscribe((currency: any) => {
+        this.currency = currency;
+        this.isLoaded = true;
+      });
+  }
+
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
   }
 
 }
