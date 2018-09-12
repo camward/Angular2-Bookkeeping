f21a92513e2aa4374083f8200ef4acbde1229b84
alex <camward@yandex.ru>
2018-09-12 22:13:39 +0400

    LazyLoad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersService } from './shared/services/users.service';
 import { AuthService } from './shared/services/auth.service';
-import { SystemModule } from './system/system.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +18,6 @@ import { SystemModule } from './system/system.module';
     HttpModule,
     AuthModule,
     AppRoutingModule,
-    SystemModule,
     BrowserAnimationsModule
   ],
   providers: [UsersService, AuthService],
